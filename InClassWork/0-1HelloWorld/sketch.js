e77efecda3f3f26d512c6463cdcb7e918bf2025b
Jo Curtis <curtissierrajo@gmail.com>
2017-02-21 16:41:30 -0700

    no message

--- InClassWork/0-1HelloWorld/sketch.js
+++ InClassWork/0-1HelloWorld/sketch.js
@@ -1,7 +1,23 @@
 function setup() {
-  console.log("Hello muh frend")
+createCanvas(600, 700);
 
 }
 function draw() {
+var bgRed, bgGreen, bgBlue
+//bgVal = mouseX * 0.425
+//text(mouseX,200, 200);
+//text(bgVal, 200, 230);
+bgRed = map(mouseX, 0, 600, 0, 255);
+bgGreen = map(mouseY, 0, 600, 0, 255);
+bgBlue = map(mouseX + mouseY 0, 1200, 255, 0);
+background(bgRed, bgGreen, bgBlue);
+
+text(mouseX, 200, 160);
+text(mouseY, 200, 180);
+text(mouseX + mouseY)
+
+
+
+
 
 }
---
+++ InClassWork/0-5HelloWorld/empty-example/sketch.js
@@ -0,0 +1,32 @@
+function setup() {
+createCanvas(800,400);
+}
+function draw() {
+background(240);
+// Technically valid
+var ellipesize = 40;
+ellipse(400, 200, x, x)
+  //variables
+  //Data Types:
+  //Numbers
+  // -integer or int
+  // -an interger is a whole number, positive or negative, including zero.
+
+var myInt = 5;
+  //Floats
+  //- a float is any REAL number with a decimal place.
+
+var myFloat = 5.0237516757
+  //Char or character
+
+var myChar = 'a';
+  //String
+  //-A string is any text, surrounded by quotations.
+var myString = "Hey dudes";
+
+
+
+
+
+
+}
