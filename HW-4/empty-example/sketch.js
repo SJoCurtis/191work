ee5113cd6769da01783cffa4e33912a41d0a6263
Jo Curtis <curtissierrajo@gmail.com>
2017-02-21 21:15:05 -0700

    no message

---
+++ HW-4/empty-example/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas(800, 800);
+background(250, 250, 250);
+}
+function draw() {
+
+
+}
--- InClassWork/0-2HelloWorld/sketch.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-createCanvas(500,500);
-background(235,200,200);
-fill(5,26,134);
-ellipse(250,250,40,40);
-}
-function draw() {
-}
