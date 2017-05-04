3e3f3f6eb7eec9ca74d9b5f25e02560c6ba49c0b
Jo Curtis <curtissierrajo@gmail.com>
2017-05-04 13:56:41 -0600

    no message

--- FinalProject/ManicPerlin/Manic2.js
+++ FinalProject/ManicPerlin/Manic2.js
@@ -7,24 +7,28 @@ var button;
 var volhistory = [];
 
 function preload() {
-  song = loadSound('./resources/FuckYourself.mp3');
-
-function setup() {
-createCanvas(windowWidth, windowHeight);
-background(255, 153, 51);
-t = 0;
-
-button = createButton('Play/Pause');
-button.mousePressed(toggleSong);
-song.play();
-amp = new p5.Amplitude();
-amp.setInput(song);
+  song = loadSound('./Resources/FuckYourself.mp3');
 }
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 166, 77);
+  t = 0;
+
+  button = createButton('Play/Pause');
+  button.mousePressed(toggleSong);
+  song.play();
+  amp = new p5.Amplitude();
+  amp.setInput(song);
+}
+
+
 function draw() {
+  push();
+  angleMode(RADIANS);
   stroke(242, 242, 242);
   noFill();
-  translate(width/2, height/2);
+  translate(width / 2, height / 2);
   beginShape();
   for (var i = 0; i < 900; i++) {
     var ang = map(i, 6, 900, 0, TWO_PI);
@@ -38,56 +42,58 @@ function draw() {
   t += 1;
 
   if (frameCount % 900 == 0) {
-	background(255, 153, 51);
+    background(255, 153, 51);
   }
-    stroke(128, 0, 0);
-    noFill();
-    translate(width/14, height/14);
-    beginShape();
-    for (var i = 0; i < 700; i++) {
-      var ang = map(i, 0, 700, 0, TWO_PI);
-      var rad = 700 * noise(i * 0.02, t * 0.005);
-      var x = rad * cos(ang);
-      var y = rad * sin(ang);
-      curveVertex(x, y);
-    }
-    endShape(CLOSE);
+  background(0, 0, 0, 15);
+  stroke(128, 0, 0);
+  noFill();
+  translate(width / 14, height / 14);
+  beginShape();
+  for (var i = 0; i < 700; i++) {
+    var ang = map(i, 0, 700, 0, TWO_PI);
+    var rad = 700 * noise(i * 0.02, t * 0.005);
+    var x = rad * cos(ang);
+    var y = rad * sin(ang);
+    curveVertex(x, y);
+  }
+  endShape(CLOSE);
 
-    t += 1;
-pop();
-// From Manic Song
-var vol = amp.getLevel();
-console.log(vol);
-volhistory.push(vol);
+  t += 1;
+  pop();
 
-push();
-blendMode(LIGHTEST);
-stroke(179, 71, 0);
-strokeWeight(6);
-fill(20);
-angleMode(DEGREES);
+  // From Manic Song
+  var vol = amp.getLevel();
+  console.log(vol);
+  volhistory.push(vol);
 
-translate(width / 2, height / 2);
-beginShape();
-for (var i = 0; i < 360; i++) {
-  var r = map(volhistory[i], 0, 1, 300, 50);
-  var x = r * cos(i);
-  var y = r * sin(i);
-  vertex(x, y);
-}
-endShape();
+  push();
+  blendMode(LIGHTEST);
+  stroke(255, 133, 51);
+  strokeWeight(6);
+  fill(20);
+  angleMode(DEGREES);
 
-if (volhistory.length > 360) {
-  volhistory.splice(0, 1);
-}
+  translate(width / 2, height / 2);
+  beginShape();
+  for (var i = 0; i < 360; i++) {
+    var r = map(volhistory[i], 0, 1, 300, 50);
+    var x = r * cos(i);
+    var y = r * sin(i);
+    vertex(x, y);
+  }
+  endShape();
 
-pop();
+  if (volhistory.length > 360) {
+    volhistory.splice(0, 1);
+  }
+
+  pop();
 }
 // Song Functions
 function toggleSong() {
-if (song.isPlaying()) {
-  song.pause();
-} else {
-  song.play();
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
   }
 }
--- FinalProject/SongShit/Deppressive.js
+++ FinalProject/SongShit/Deppressive.js
@@ -4,6 +4,8 @@ var song;
 var amp;
 var button;
 
+var section = 0;
+
 var volhistory = [];
 
 function preload() {
@@ -23,69 +25,136 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0, 0, 15);
-  push();
-  angleMode(RADIANS);
-  stroke(166, 166, 166);
-  noFill();
-  translate(width / 2, height / 2);
-  beginShape();
-  for (var i = 0; i < 800; i++) {
-    var ang = map(i, 6, 800, 0, TWO_PI);
-    var rad = 800 * noise(i * 0.01, t * 0.005);
-    var x = rad * cos(ang);
-    var y = rad * sin(ang);
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
-
-  t += 1;
-
-if (frameCount % 900 == 0) {
-    background(0);
-  }
-  stroke(0, 51, 102);
-  noFill();
-  translate(width / 13, height / 13);
-  beginShape();
-  for (var i = 0; i < 700; i++) {
-    var ang = map(i, 0, 700, 0, TWO_PI);
-    var rad = 700 * noise(i * 0.01, t * 0.005);
-    var x = rad * cos(ang);
-    var y = rad * sin(ang);
-    curveVertex(x, y);
-  }
-  endShape(CLOSE);
-  t += 1;
-  pop();
-
-// From Depressive Song
+  // From Depressive Song
   var vol = amp.getLevel();
-  console.log(vol);
+  // console.log(vol);
   volhistory.push(vol);
 
-  push();
-  blendMode(LIGHTEST);
-  stroke(100, 157, 206);
-  strokeWeight(6);
-  fill(20);
-  angleMode(DEGREES);
 
-  translate(width / 2, height / 2);
-  beginShape();
-  for (var i = 0; i < 360; i++) {
-    var r = map(volhistory[i], 0, 1, 300, 50);
-    var x = r * cos(i);
-    var y = r * sin(i);
-    vertex(x, y);
+
+  if (section === 0) {
+    background(0, 0, 0, 15);
+    push();
+    angleMode(RADIANS);
+    stroke(166, 166, 166);
+    noFill();
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 800; i++) {
+      var ang = map(i, 6, 800, 0, TWO_PI);
+      var rad = 800 * noise(i * 0.01, t * 0.005);
+      var x = rad * cos(ang);
+      var y = rad * sin(ang);
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+
+    t += 1;
+
+    if (frameCount % 900 == 0) {
+      background(0);
+    }
+    stroke(0, 51, 102);
+    noFill();
+    translate(width / 13, height / 13);
+    beginShape();
+    for (var i = 0; i < 700; i++) {
+      var ang = map(i, 0, 700, 0, TWO_PI);
+      var rad = 700 * noise(i * 0.01, t * 0.005);
+      var x = rad * cos(ang);
+      var y = rad * sin(ang);
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+    t += 1;
+    pop();
+
+
+    push();
+    blendMode(LIGHTEST);
+    stroke(100, 157, 206);
+    strokeWeight(6);
+    fill(20);
+    angleMode(DEGREES);
+
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 360; i++) {
+      var r = map(volhistory[i], 0, 1, 300, 50);
+      var x = r * cos(i);
+      var y = r * sin(i);
+      vertex(x, y);
+    }
+    endShape();
+    pop();
+  } else {
+    push();
+    angleMode(RADIANS);
+    stroke(242, 242, 242);
+    noFill();
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 900; i++) {
+      var ang = map(i, 6, 900, 0, TWO_PI);
+      var rad = 800 * noise(i * 0.02, t * 0.005);
+      var x = rad * cos(ang);
+      var y = rad * sin(ang);
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+
+    t += 1;
+
+    if (frameCount % 900 == 0) {
+      background(255, 153, 51);
+    }
+    background(0, 0, 0, 15);
+    stroke(128, 0, 0);
+    noFill();
+    translate(width / 14, height / 14);
+    beginShape();
+    for (var i = 0; i < 700; i++) {
+      var ang = map(i, 0, 700, 0, TWO_PI);
+      var rad = 700 * noise(i * 0.02, t * 0.005);
+      var x = rad * cos(ang);
+      var y = rad * sin(ang);
+      curveVertex(x, y);
+    }
+    endShape(CLOSE);
+
+    t += 1;
+    pop();
+
+
+    push();
+    blendMode(LIGHTEST);
+    stroke(255, 133, 51);
+    strokeWeight(6);
+    fill(20);
+    angleMode(DEGREES);
+
+    translate(width / 2, height / 2);
+    beginShape();
+    for (var i = 0; i < 360; i++) {
+      var r = map(volhistory[i], 0, 1, 300, 50);
+      var x = r * cos(i);
+      var y = r * sin(i);
+      vertex(x, y);
+    }
+    endShape();
+
+
+
+    pop();
   }
-  endShape();
+
+
+
 
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
   }
 
-  pop();
 }
 // Song Functions
 function toggleSong() {
